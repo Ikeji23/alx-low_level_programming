2d14cc8da319aeae6a3d68e21a50ffaed470b5ce
Chinyere Priscilla Ikeji <122909325+Ikeji23@users.noreply.github.com>
2023-02-21 21:53:43 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,6 +12,7 @@
 int _putchar(char c);
 int main(void)
 {
+    _putchar(' ');
     _putchar('p');
     _putchar('u');
     _putchar('t');
@@ -20,6 +21,5 @@ int main(void)
     _putchar('a');
     _putchar('r');
     _putchar('\n');
-
     return (0);
 }
