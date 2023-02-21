97daa8cee4fd17c70ec3fef7a824f809bda8501f
Chinyere Priscilla Ikeji <Chinyereikeji136@gmail.com>
2023-02-21 14:40:16 -0800

    Suffer from insomnia

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,19 @@
 #include "main.h"
 
 /**
- * main - prints the alphabet
- * Description: print the alphabet in lower case
- * Return: Always (Success)
- */
-main print_alphabet(void)
+ * print_alphabet - returns nothing
+ *
+ * _putchar - prints character
+ *
+ * Return: Always 0
+ * */
+void print_alphabet(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
+	{
 		_putchar(c);
+	}
 	_putchar('\n');
 }
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-prints alphabet
+void
