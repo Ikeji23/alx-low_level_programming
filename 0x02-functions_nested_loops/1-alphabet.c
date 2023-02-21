451963410f3350869a9f36a7688bef5fd92edaac
Chinyere Priscilla Ikeji <122909325+Ikeji23@users.noreply.github.com>
2023-02-21 23:14:26 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,15 @@
 #include "main.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * main - prints the alphabet
+ * Description: print the alphabet in lower case
+ * Return: Always (Success)
  */
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
 }
