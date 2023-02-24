5a2ee35a6a7bba4261bdc380c69b576a71bcf1be
Chinyere Priscilla Ikeji <Chinyereikeji136@gmail.com>
2023-02-23 16:02:20 -0800

    Straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - draw a straight line
+ * @n: the number of lines
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
