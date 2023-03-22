facaeae5ae84a5a78d78ceec9230e80b51e49f2b
Chinyere Priscilla Ikeji <Chinyereikeji136@gmail.com>
2023-03-22 14:26:42 -0700

    Whats my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print the name
+ *
+ * @name: the string (a name in this case) to print
+ * @func: pointer to a function that accepts a string as argument
+ * and returns void
+ *
+ */
+void print_name(char *name, void (*func)(char *))
+{
+	if (name && func)
+		func(name);
+}
