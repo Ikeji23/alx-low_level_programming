cd22ad05aae7221b7828cd9b90dd5b87a7c1ab2a
Chinyere Priscilla Ikeji <122909325+Ikeji23@users.noreply.github.com>
2023-04-04 00:00:17 +0100

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,7 +3,6 @@
 /**
  * listint_len - Returns the number of elements in a listint_t list.
  * @h: A pointer to the head of the listint_t list.
- *
  * Return: The number of nodes in the listint_t list.
  */
 size_t listint_len(const listint_t *h)
