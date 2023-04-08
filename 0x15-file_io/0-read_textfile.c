ba7afea54c2f06f9d3cb81b2b7497476cda19e27
Chinyere Priscilla Ikeji <chinyereikeji136@gmail.com>
2023-04-08 03:17:37 -0700

    Fifth time is a charm

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,60 +1,57 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the
- * POSIX standard output
- *
- * @filename: file to read
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
  * @letters: number of letters to read and print
  *
- * Return: actual number of letters it could read and print, otherwise
- *		0 if file cannot be opened or read, @filename is NULL
- *		or if write fails or does not write the expected
- *		amount of bytes
+ * Return: actual number of letters it could read and print, otherwise 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	char *buffer;
-	ssize_t read_count, write_count;
-
-	if (!filename)
+	if (filename == NULL)
+	{
 		return (0);
+	}
 
-	fd = open(filename, O_RDONLY);
+	int fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (!buffer)
+	{
+		return (0);
+	}
+
+	char *buff = malloc(letters);
+	if (buff == NULL)
+
 	{
 		close(fd);
 		return (0);
 	}
 
-	read_count = read(fd, buffer, letters);
-	if (read_count == -1)
-	{
+	 ssize_t nread = read(fd, buf, letters);
+	 if (nread == -1)
+
+	 {
 		close(fd);
-		free(buffer);
+		free(buff);
 		return (0);
-	}
+	 }
 
-	write_count = write(STDOUT_FILENO, buffer, read_count);
-	if (write_count == -1 || (size_t)write_count != read_count)
-	{
-		close(fd);
-		free(buffer);
-		return (0);
-	}
+	 ssize_t nwritten = write(STDOUT_FILENO, buf, nread);
+	 if (nwritten == -1 || nwritten != nread)
+	 {
+		 close(fd);
+		 free(buff);
+		 return (0);
+	 }
 
-	close(fd);
-	free(buffer);
+	 close(fd);
+	 free(buff);
 
-	return (write_count);
+	 return nread;
 }
