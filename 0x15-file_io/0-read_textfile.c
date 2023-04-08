b007cb7e0db70224454a41f6241c411117cfa186
Chinyere Priscilla Ikeji <chinyereikeji136@gmail.com>
2023-04-08 02:34:48 -0700

    I hope this works

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,43 +1,59 @@
-#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <stdio>
+#include <unistd.h>
 #include <fcntl.h>
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to the
+ * read_textfile - reads a text file and prints it to the
  * POSIX standard output
  *
- * @filename: File to read
- * @letters: Number of letters to read and write
+ * @filename: file to read
+ * @letters: number of letters to read and print
  *
- * Retrun: Actual number of letters it could read and print, otherwise
- *		return 0 if the file cannot be read or opened, @filename
- * 		is NULL
- * 		or if write fails or does not write the expected amount
- * 		of bytes
- */ 		
-ssize_t read_textfile(const char *filename, size_t letters);
+ * Return: actual number of letters it could read and print, otherwise
+ *		0 if file cannot be opened or read, @filename is NULL
+ *		or if write fails or does not write the expected
+ *		amount of bytes
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, printed, wrote;
-	char *buff;
+	int fd;
+	char *buffer;
+	ssize_t read_count, write_count;
 
-	buff = malloc(letters);
-	if (buff == NULL)
+	if (!filename)
 		return (0);
-	if (filename == NULL)
-		return (0);
-	fd = open(filename, O_RDWR);
+
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	printed = read(fd, buff, letters);
-	if (printed == -1)
+
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+	{
+		close(fd);
 		return (0);
-	wrote = write(STDOUT_FILENO, buff, printed);
-	if (wrote == -1)
+	}
+
+	read_count = read(fd, buffer, letters);
+	if (read_count == -1)
+	{
+		close(fd);
+		free(buffer);
 		return (0);
-	if (close(fd) == -1)
+	}
+
+	write_count = write(STDOUT_FILENO, buffer, read_count);
+	if (write_count == -1 || (size_t)write_count != read_count)
+	{
+		close(fd);
+		free(buffer);
 		return (0);
-	free(buff);
-	return (printed);
+	}
+
+	close(fd);
+	free(buffer);
+
+	return (write_count);
 }
